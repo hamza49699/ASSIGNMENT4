67c0a0d15fed321b0e9a6f0f34e101d4dac09063
Hamza Javed <159588350+hamza49699@users.noreply.github.com>
2025-04-04 18:18:56 +0500

    Add files via upload

---
+++ assignment00_to_05/01_expressions/03_feet_to_inches.py
@@ -0,0 +1,10 @@
+INCHES_IN_FOOT: int = 12  # Conversion factor. There are 12 inches for 1 foot.
+
+def main():
+    feet: float = float(input("Enter number of feet: "))  # Get the number of feet, make sure to cast it to a float!
+    inches: float = feet * INCHES_IN_FOOT  # Perform the conversion
+    print("That is", inches, "inches!")
+
+
+if __name__ == '__main__':
+    main()
