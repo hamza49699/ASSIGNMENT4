39b59e1ee82c561a35af3f7c706306ae8c4dee9f
Hamza Javed <159588350+hamza49699@users.noreply.github.com>
2025-04-04 18:48:51 +0500

    Add files via upload

---
+++ assignment01_online_projects/basics/02_liftoff.py
@@ -0,0 +1,3 @@
+for i in range(10, 0, -1):  # Starts from 10, ends at 1, decrementing by 1
+    print(i, end=" ")  # Print numbers on the same line with a space
+print("Liftoff!")  # Print "Liftoff!" after the countdown
